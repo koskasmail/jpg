4662687ed799c4ef7576cd127cd6420d41767cf8
kessler yaron <koskasmail@gmail.com>
2022-06-29 16:04:04 +0300

    Boolean True False Test

---
+++ data/variable/BooleanTrueFalse.java
@@ -0,0 +1,18 @@
+
+public class LogicTrick {
+
+	public LogicTrick() {
+
+		boolean value = false;
+
+		// == false
+		if (value == false) {
+			System.out.println("value == false");
+		}
+
+		// == false
+		if (!value) {
+			System.out.println("!value == " + !value);
+		}
+	}
+}
