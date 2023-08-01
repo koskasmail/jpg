17fde48e2aa78ac8aff127500e2c97c56b05ea4a
kessler yaron <koskasmail@gmail.com>
2023-08-01 16:53:06 +0300

    java create service that get all document infromation from folder

---
+++ os/DocumentService.java
@@ -0,0 +1,37 @@
+/***
+* DocumentService --> navigator java create service that get all document infromation from folder
+**/
+
+import java.io.File;
+
+public class DocumentService {
+
+    public static void main(String[] args) {
+        String folderPath = "path/to/your/folder"; // Replace with the actual folder path
+
+        File folder = new File(folderPath);
+        if (folder.exists() && folder.isDirectory()) {
+            processFolderContents(folder);
+        } else {
+            System.out.println("Folder not found or is not a directory.");
+        }
+    }
+
+    private static void processFolderContents(File folder) {
+        File[] files = folder.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    // Process document information here (e.g., get name, size, last modified date, etc.)
+                    System.out.println("File name: " + file.getName());
+                    System.out.println("File size: " + file.length() + " bytes");
+                    System.out.println("Last modified: " + new Date(file.lastModified()));
+                    System.out.println("--------------");
+                } else if (file.isDirectory()) {
+                    // If the file is a subdirectory, recursively process its contents
+                    processFolderContents(file);
+                }
+            }
+        }
+    }
+}
